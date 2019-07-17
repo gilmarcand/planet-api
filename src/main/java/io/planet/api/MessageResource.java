2341586a31edc11827958a708e204b63bef237f2
Gilmar Candido <gilmarcand@gmail.com>
2019-07-16 22:34:29 -0300

    refinando aplicação

--- src/main/java/io/planet/api/MessageResource.java
+++ src/main/java/io/planet/api/MessageResource.java
@@ -1,62 +1,32 @@
 package io.planet.api;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public class MessageResource {
 
-    public static final int ERROR = 1;
-    public static final int WARNING = 2;
-    public static final int INFO = 3;
-    public static final int OK = 4;
+    public static final int ERROR = 0;
+    public static final int INFO = 1;
 
-    int code;
-    String type;
-    String message;
-
-    public MessageResource(){}
+    private String type;
+    private int code;
+    private String message;
 
     public MessageResource(int code, String message){
         this.code = code;
         switch(code){
             case ERROR:
-                setType("error");
-                break;
-            case WARNING:
-                setType("warning");
+                this.type = "error";
                 break;
             case INFO:
-                setType("info");
-                break;
-            case OK:
-                setType("ok");
+                this.type = "info";
                 break;
             default:
-                setType("unknown");
+                this.type = "unknown";
                 break;
         }
         this.message = message;
     }
 
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
